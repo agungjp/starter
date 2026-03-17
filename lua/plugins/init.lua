49d48433b0f0673766ba0a9076b258fa219821ea
agungjp <agungjp@gmail.com>
2026-03-17 10:49:22 +0700

    feat: add snacks.nvim (claudecode dependency)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,29 @@ return {
     end,
   },
 
+  {"christoomey/vim-tmux-runner"},
+
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    config = function()
+      require("smart-splits").setup({
+        -- Multiplexer integration: tmux
+        multiplexer_integration = "tmux",
+      })
+      -- Navigasi pane: Ctrl+h/j/k/l (override nvim default, extend ke tmux)
+      vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left,  { desc = "move to left split/pane" })
+      vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down,  { desc = "move to lower split/pane" })
+      vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up,    { desc = "move to upper split/pane" })
+      vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "move to right split/pane" })
+      -- Resize pane: Alt+arrow
+      vim.keymap.set("n", "<A-Left>",  require("smart-splits").resize_left,  { desc = "resize left" })
+      vim.keymap.set("n", "<A-Down>",  require("smart-splits").resize_down,  { desc = "resize down" })
+      vim.keymap.set("n", "<A-Up>",    require("smart-splits").resize_up,    { desc = "resize up" })
+      vim.keymap.set("n", "<A-Right>", require("smart-splits").resize_right, { desc = "resize right" })
+    end,
+  },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
@@ -22,4 +45,14 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      terminal = { enabled = true },
+      notifier = { enabled = true },
+    },
+  },
 }
